1de5960ae84259cc347becebb16f4e4f32002366
ahmed <ahmedehsouna@yahoo.com>
2020-01-24 21:46:17 +0100

    workin on auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./components/client/home/home.component";
+import { AuthGuard } from "./guards/auth.guard";
+import { EntranceComponent } from "./components/guest/entrance/entrance.component";
 
-const routes: Routes = [{ path: "", component: HomeComponent }];
+const routes: Routes = [
+  { path: "", component: HomeComponent, canActivate: [AuthGuard] },
+  { path: "", component: EntranceComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
