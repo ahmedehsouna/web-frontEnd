6494e675743ce1493a13eada40ff28d613167df7
Noah <ahmedehsouna@yahoo.com>
2020-01-31 12:57:34 +0100

    working on home

--- src/app/components/client/home/home.component.ts
+++ src/app/components/client/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http/http.service';
 import { Observable } from 'rxjs';
+import { DataService } from 'src/app/services/data/data.service';
 
 @Component({
   selector: 'app-home',
@@ -9,17 +10,18 @@ import { Observable } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private http : HttpService) { }
-  car:String = "car"
-  another:String = "another";
-  $posts:Observable<any>;
-  getCar(){
-    console.log("hello")
-    return this.car
+  constructor(private http : HttpService,private data:DataService) { }
 
-  }
+  $posts:Observable<any>;
   ngOnInit() {
-    this.$posts = this.http.get('/posts')
+    this.data.Community.subscribe(community=>{
+      this.$posts = this.http.get('/posts')
+      this.$posts.subscribe(data=>{
+        console.log(data)
+      })
+
+    })
+    // this.$posts = this.http.get('/posts')
   }
 
 }
