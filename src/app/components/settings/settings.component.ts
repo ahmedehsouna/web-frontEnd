791ffa45778d380187caaa3495b0f9aa06ee704d
rue-elwahishi <rubawahishi@gmail.com>
2020-02-07 10:59:48 +0100

    profile finishes

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -10,6 +10,7 @@ export class SettingsComponent implements OnInit {
   constructor(private http: HttpService) {}
   newData: any;
   user: any;
+  fileData: any;
 
   ngOnInit() {
     this.http.get(`/users/${localStorage.username}`).subscribe((one: any) => {
@@ -23,7 +24,15 @@ export class SettingsComponent implements OnInit {
     var formData = new FormData(form);
 
     this.http.patch(`/users/settings`, formData).subscribe(data => {
-      console.log(data);
+      console.log(data, "hiii");
     });
+  }
+  upload(e: any, div) {
+    var input = e.target;
+
+    if (input.files && input.files[0]) {
+      div.style["background-image"] =
+        "url(" + URL.createObjectURL(input.files[0]) + ")";
+    }
   }
 }
